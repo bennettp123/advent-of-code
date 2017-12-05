5fa67ab900fda29af3d0e62c18ebf237f87ddcbf
Bennett Perkins <bennettperkins@gmail.com>
2017-12-05 19:46:06 +0800

    implement 2015 day 2 part 2

--- 2015/2015-day02/box.py
+++ 2015/2015-day02/box.py
@@ -14,16 +14,33 @@ def paper_needed(w, h, l):
     return box_area(w, h, l) + smallest_side_area(w, h, l)
 
 
+def smallest_side_perimeter(w, l, h):
+    sides = (2*(w+h), 2*(w+l), 2*(h+l))
+    return min(sides)
+
+
+def box_volume(w, l, h):
+    return w * h * l
+
+
+def ribbon_needed(w, l, h):
+    return smallest_side_perimeter(w, l, h) + box_volume(w, l, h)
+
+
 if __name__ == "__main__":
 
-    total_needed = 0
+    total_paper = 0
+    total_ribbon = 0
 
     with open('input', 'r') as f:
 
         for line in f:
             w, l, h = [int(x) for x in line.split('x')]
-            total_needed = total_needed + paper_needed(w, h, l)
+            total_paper = total_paper + paper_needed(w, h, l)
+            total_ribbon = total_ribbon + ribbon_needed(w, l, h)
 
-    print('total needed: {0}'.format(total_needed))
+
+    print('total paper needed: {0}'.format(total_paper))
+    print('total ribbon needed: {0}'.format(total_ribbon))
 
 
