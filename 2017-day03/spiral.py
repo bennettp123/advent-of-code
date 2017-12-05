425620d5126dac2563485b0f7bad66968ef4b888
Bennett Perkins <bennettperkins@gmail.com>
2017-12-05 14:14:38 +0800

    update spiral.py for day03 part 2

--- 2017-day03/spiral.py
+++ 2017-day03/spiral.py
@@ -35,11 +35,37 @@ def spiral_coords(size = sys.maxsize):
 if __name__ == '__main__':
     idx = 0
 
+    buf = [[None for i in range(0,1000)] for i in range(0,1000)]
+    # origin = (500, 500)
+
     spiral = list(spiral_coords(312051))
 
+    part2_found = False
     for coords in spiral:
         x, y = coords
         idx = idx + 1
+
+        x1 = x + 500
+        y1 = y + 500
+
+        adjacent_coords = [ (x1 + 1, y1), (x1 + 1, y1 + 1),
+                            (x1, y1 + 1), (x1 - 1, y1  + 1),
+                            (x1 - 1, y1), (x1 - 1, y1 - 1),
+                            (x1, y1 - 1), (x1 + 1, y1 - 1) ]
+
+        adjacent_values = [buf[x][y] for x, y in adjacent_coords
+                if not buf[x][y] is None]
+
+        if not adjacent_values:
+            buf[x1][y1] = 1
+        else:
+            buf[x1][y1] = sum(adjacent_values)
+
+        if buf[x1][y1] > 312051:
+            if not part2_found:
+                print("part2: first sum greater than 312051 is {0}".format(
+                    buf[x1][y1]))
+                part2_found = True
 
     print("part1: distance is {0}".format(abs(x)+abs(y)))
 
