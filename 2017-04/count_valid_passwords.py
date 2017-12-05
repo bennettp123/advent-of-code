7da37ce3aad24f3b389f2dc6ca301a93beae16b3
Bennett Perkins <bennettperkins@gmail.com>
2017-12-05 08:45:20 +0800

    Revert "update day 4 to support part 2"
    This reverts commit 7fce2427935c98fb3d0f5e0e252c999cf9233697.

--- 2017-04/count_valid_passwords.py
+++ 2017-04/count_valid_passwords.py
@@ -1,30 +1,16 @@
 #!/usr/bin/env python
 
+import os
 import sys
-import itertools
 
 
-def permutations(string):
-    permutations = [''.join(x) for x in itertools.permutations(string)]
-    unique_permutations = set(permutations)
-    return list(unique_permutations)
-
-
-def all_anagrams(array):
-    anagrams = []
-    for word in array:
-        anagrams = anagrams + permutations(word)
-    return anagrams
-
-
-def is_valid(array):
-    anagrams = all_anagrams(array)
-    return len(anagrams) == len(set(anagrams))
+def is_unique(array):
+    return len(array) == len(set(array))
 
 
 if __name__ == '__main__':
     passwords = (line for line in sys.stdin)
-    valid_passwords = (p for p in passwords if is_valid(p.split()))
+    valid_passwords = (p for p in passwords if is_unique(p.split()))
     count = 0
     for password in valid_passwords:
         count = count + 1
