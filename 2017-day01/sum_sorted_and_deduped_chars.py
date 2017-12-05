e5efacb0058fe98d2a539989843f8f4c4774e217
Bennett Perkins <bennettperkins@gmail.com>
2017-12-05 11:37:39 +0800

    add working solution for day 1 part 2

--- 2017-day01/sum_sorted_and_deduped_chars.py
+++ 2017-day01/sum_sorted_and_deduped_chars.py
@@ -4,7 +4,9 @@ import sys
 
 if __name__ == '__main__':
 
-    total = 0
+    part1_total = 0
+    part2_total = 0
+    array = []
     cur = None
     prev = None
     first = None
@@ -24,20 +26,32 @@ if __name__ == '__main__':
                 # ignore whitespace, unicode BOMs, alphabetic, etc
                 continue
 
+            # build an array
+            array = array + [cur]
+
             if first is None:
                 first = cur
 
+            # caulculate part1_total
             if prev is not None:
                 if prev == cur:
-                    total = total + prev
+                    part1_total = part1_total + prev
 
             prev = cur
 
-    # handle wraparound
+    # part1: handle wraparound
     if prev is not None:
         if first is not None:
             if first == cur:
-                total = total + first
+                part1_total = part1_total + first
 
-    print("total is {0}".format(total))
+    # calculate part2_total
+    offset = len(array) / 2
+    for digit in range(0, len(array)):
+        num = array[digit]
+        if num == array[(digit + offset) % len(array)]:
+            part2_total = part2_total + num
+
+    print("part1 total is {0}".format(part1_total))
+    print("part2 total is {0}".format(part2_total))
 
