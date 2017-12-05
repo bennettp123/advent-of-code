ed490b2fbe448bcd32d81bcd2ad77c22e0f6a771
Bennett Perkins <bennettperkins@gmail.com>
2017-12-05 18:57:41 +0800

    2015 day 01 part 2

--- 2015/2015-day01/an_easy_warmup.py
+++ 2015/2015-day01/an_easy_warmup.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
+import sys
+
 if __name__ == '__main__':
 
     floor = 0
+    counter = 0
     with open('input', 'r') as f:
         for line in f:
-            ups = len([x for x in line if x == '('])
-            downs = len([x for x in line if x == ')'])
-            floor = floor + ups - downs
-
-    print("on floor {0}".format(floor))
+            for char in line:
+                counter = counter + 1
+                if char == '(':
+                    floor = floor + 1
+                if char == ')':
+                    floor = floor - 1
+                if floor < 0:
+                    print("on floor {0}, at counter {1}".format(floor, counter))
+                    sys.exit(0)
 
