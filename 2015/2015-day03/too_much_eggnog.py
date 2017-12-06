0fe412ea15d8b2d416258d2fd7ffb8fcb0b846cd
Bennett Perkins <bennettperkins@gmail.com>
2017-12-06 14:29:50 +0800

    2015 day 03 part 2, i think? committing anyway

--- 2015/2015-day03/too_much_eggnog.py
+++ 2015/2015-day03/too_much_eggnog.py
@@ -9,22 +9,24 @@ if __name__ == '__main__':
             "^": (0, 1),
             "v": (0, -1)}
 
-    pos = "0 0"
-    visited_locations[pos] = 1
+    pos = ["0 0", "0 0"]
+    visited_locations["0 0"] = 2
+    is_robosanta = 0
 
     with open('input', 'r') as f:
         for line in f:
             for char in line:
-                x, y = [int(x) for x in pos.split()]
+                is_robosanta = (is_robosanta + 1) % 2
+                x, y = [int(x) for x in pos[is_robosanta].split()]
                 try:
                     x_delta, y_delta = dirs[char]
                 except KeyError:
                     # ignore whitespace, unicode BOMs, etc
                     continue
-                pos = "{0} {1}".format(x + x_delta, y + y_delta)
-                if pos not in visited_locations:
-                    visited_locations[pos] = 0
-                visited_locations[pos] += 1
+                pos[is_robosanta] = "{0} {1}".format(x + x_delta, y + y_delta)
+                if pos[is_robosanta] not in visited_locations:
+                    visited_locations[pos[is_robosanta]] = 0
+                visited_locations[pos[is_robosanta]] += 1
 
-    print('part1: {0} houses visited'.format(len(visited_locations)))
+    print('part2: {0} houses visited'.format(len(visited_locations)))
 
