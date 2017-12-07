d8905d59cb9a29596992145f661492def0da9099
Bennett Perkins <bennettperkins@gmail.com>
2017-12-07 15:51:01 +0800

    2015 day 05 part 2

--- 2015/2015-day05/nice.py
+++ 2015/2015-day05/nice.py
@@ -21,12 +21,26 @@ def is_nice(s):
     return len(vowels(s)) > 2 and repeats(s) and not blacklisted(s)
 
 
+def repeater(s):
+    return re.search(r'(..).*\1', s)
+
+
+def repleater(s):
+    return re.search(r'(.).\1', s)
+
+
+def is_nicer(s):
+    return repeater(s) and repleater(s)
+
+
 if __name__ == '__main__':
     with open('input', 'r') as f:
         for line in f:
             strings = strings + [line]
 
     nice_strings = [s for s in strings if is_nice(s)]
+    nicer_strings = [s for s in strings if is_nicer(s)]
 
     print('part 1: {0} nice strings found'.format(len(nice_strings)))
+    print('part 2: {0} nicer strings found'.format(len(nicer_strings)))
 
