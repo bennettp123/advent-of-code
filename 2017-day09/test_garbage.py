18ec9ea2aec783c85b4fa2e2dfc510f8533a63a6
Bennett Perkins <bennettperkins@gmail.com>
2017-12-10 08:41:45 +0800

    add more tests after reading the requirements

--- 2017-day09/test_garbage.py
+++ 2017-day09/test_garbage.py
@@ -32,12 +32,22 @@ group_strings = [
         '{{<a>},{<a>},{<a>},{<a>}}',
         '{{<!>},{<!>},{<!>},{<a>}}']
 
+groups_with_scores = [
+        ('{}', 1),
+        ('{{{}}}', 6),
+        ('{{},{}}', 5),
+        ('{{{},{},{{}}}}', 16),
+        ('{<a>,<a>,<a>,<a>}', 1),
+        ('{{<ab>},{<ab>},{<ab>},{<ab>}}', 9),
+        ('{{<!!>},{<!!>},{<!!>},{<!!>}}', 9),
+        ('{{<a!>},{<a!>},{<a!>},{<ab>}}', 3)]
+
 
 def get_testmethod(s):
     def test_process(self):
         expected_startpos = 0
         expected_endpos = len(s) - 1
-        actual_startpos, actual_endpos = garbage.process(s)
+        actual_startpos, actual_endpos, _ = garbage.process(s)
         self.assertEqual(expected_startpos, actual_startpos)
         self.assertEqual(expected_endpos, actual_endpos)
     return test_process
@@ -55,6 +65,19 @@ for n in range(len(group_strings)):
     test_method.__name__ = 'test_process_group_{0}'.format(n)
     setattr(GroupTests, test_method.__name__, test_method)
 
+
+def get_testmethod_with_score(s, expected_score):
+    def test_process(self):
+        _, _, actual_score = garbage.process(s)
+        self.assertEqual(expected_score, actual_score)
+    return test_process
+
+
+for n in range(len(groups_with_scores)):
+    group, expected_score = groups_with_scores[n]
+    test_method = get_testmethod_with_score(group, expected_score)
+    test_method.__name__ = 'test_process_group_with_score_{0}'.format(n)
+    setattr(GroupTests, test_method.__name__, test_method)
 
 if __name__ == '__main__':
     unittest.main()
