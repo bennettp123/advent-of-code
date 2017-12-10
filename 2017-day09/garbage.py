1679f44dc603fd5af041e81527845973aba410ff
Bennett Perkins <bennettperkins@gmail.com>
2017-12-10 08:55:28 +0800

    2017 day 09 part 1

--- 2017-day09/garbage.py
+++ 2017-day09/garbage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-score = 0
 
 def process(s, depth=0):
     if s.startswith('<'):
-        # garbage
+        # is garbage
         pos = 0
         while True:
             pos += 1
@@ -16,7 +15,7 @@ def process(s, depth=0):
                 endpos = pos
                 return 0, endpos, 0
     if s.startswith('{'):
-        # group
+        # is group
         score = depth + 1
         pos = 0
         while True:
@@ -26,9 +25,12 @@ def process(s, depth=0):
                 pos += 1
                 continue
             if s[pos] == '<':
-                pos = pos + process(s[pos:])[1]
+                # skip garbage
+                _, pos_delta, _ = process(s[pos:])
+                pos += pos_delta
                 continue
             if s[pos] == '{':
+                # process inner group
                 _, pos_delta, score_delta = process(s[pos:], depth=depth+1)
                 pos += pos_delta
                 score += score_delta
@@ -39,10 +41,11 @@ def process(s, depth=0):
 
 
 if __name__ == '__main__':
-    totalscore = 0
+    total = 0
     with open('input', 'r') as f:
         for line in f:
             _, _, score = process(line)
+            total += score
 
-    print('part 1: total group score is {0}'.format(totalscore))
+    print('part 1: total group score is {0}'.format(total))
 
