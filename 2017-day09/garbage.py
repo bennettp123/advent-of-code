3ebf4a36a466a7c1e1499af71b1ff954691ad4d4
Bennett Perkins <bennettperkins@gmail.com>
2017-12-10 09:18:39 +0800

    2017 day 09 part 2

--- 2017-day09/garbage.py
+++ 2017-day09/garbage.py
@@ -5,19 +5,23 @@ def process(s, depth=0):
     if s.startswith('<'):
         # is garbage
         pos = 0
+        garbage_count = 0
         while True:
             pos += 1
             if s[pos] == '!':
                 # skip next char
                 pos += 1
+                garbage_count += -2
                 continue
             if s[pos] == '>':
                 endpos = pos
-                return 0, endpos, 0
+                garbage_count += endpos - 1
+                return 0, endpos, 0, garbage_count
     if s.startswith('{'):
         # is group
         score = depth + 1
         pos = 0
+        garbage_count = 0
         while True:
             pos += 1
             if s[pos] == '!':
@@ -26,26 +30,32 @@ def process(s, depth=0):
                 continue
             if s[pos] == '<':
                 # skip garbage
-                _, pos_delta, _ = process(s[pos:])
+                _, pos_delta, _, garbage_delta = process(s[pos:])
                 pos += pos_delta
+                garbage_count += garbage_delta
                 continue
             if s[pos] == '{':
                 # process inner group
-                _, pos_delta, score_delta = process(s[pos:], depth=depth+1)
+                _, pos_delta, score_delta, garbage_delta = process(
+                        s[pos:], depth=depth+1)
                 pos += pos_delta
                 score += score_delta
+                garbage_count += garbage_delta
                 continue
             if s[pos] == '}':
                 endpos = pos
-                return 0, endpos, score
+                return 0, endpos, score, garbage_count
 
 
 if __name__ == '__main__':
-    total = 0
+    total_score = 0
+    total_garbage = 0
     with open('input', 'r') as f:
         for line in f:
-            _, _, score = process(line)
-            total += score
+            _, _, score, garbage = process(line)
+            total_score += score
+            total_garbage += garbage
 
-    print('part 1: total group score is {0}'.format(total))
+    print('part 1: total group score is {0}'.format(total_score))
+    print('part 2: total garbage is {0}'.format(total_garbage))
 
