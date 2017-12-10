27d1622ce029ef5f8c18d7a1e3647658dbc572f3
Bennett Perkins <bennettperkins@gmail.com>
2017-12-10 14:14:25 +0800

    clean up a bit

--- 2017-day10/knot_hap.py
+++ 2017-day10/knot_hap.py
@@ -3,29 +3,36 @@
 import operator
 
 
+input_string = '189,1,111,246,254,2,0,120,215,93,255,50,84,15,94,62'
+
+
 def main():
 
     # part 1
     nums = range(256)
     pos = 0
     skip_size = 0
-    lengths = '189,1,111,246,254,2,0,120,215,93,255,50,84,15,94,62'.split(',')
-    nums, pos, skip_size = round(nums, pos, skip_size, lengths)
+    lengths = input_string.split(',')
+
+    for _ in range(1):
+        nums, pos, skip_size = round(nums, pos, skip_size, lengths)
+
     print('part 1: product of first two numbers is {0}'.format(nums[0]*nums[1]))
 
     # part 2
     nums = range(256)
     pos = 0
     skip_size = 0
-    lengths = [ord(x) for x in '189,1,111,246,254,2,0,120,215,93,255,50,84,15,94,62'] + [17, 31, 73, 47, 23]
+    lengths = [ord(x) for x in input_string] + [17, 31, 73, 47, 23]
+
     for _ in range(64):
         nums, pos, skip_size = round(nums, pos, skip_size, lengths)
 
     sparse_hash = nums
     dense_hash = [reduce(operator.xor, sparse_hash[x:16+x])
                     for x in range(0, len(sparse_hash), 16)]
-
     hex_string = ''.join(['{:02x}'.format(int(c)) for c in dense_hash])
+
     print('part 2: hex string is {0}'.format(hex_string))
 
 
