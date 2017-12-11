be156da35c1d4c380898eef761873bc88f312654
Bennett Perkins <bennettperkins@gmail.com>
2017-12-11 15:57:22 +0800

    shorten names for readability

--- 2017-day11/hex_but_square.py
+++ 2017-day11/hex_but_square.py
@@ -1,36 +1,33 @@
 #!/usr/bin/env python
 
-xpos = 0
-ypos = 0
-maxdist = 0
+x = 0
+y = 0
+maxd = 0
 
-def move(x, y, direction):
+def move(x, y, d):
     directions = {'n': (0, 1), 'ne': (1, 1), 'se': (1, 0), 's': (0, -1),
             'sw': (-1, -1), 'nw': (-1, 0)}
-    x_delta, y_delta = directions[direction]
+    x_delta, y_delta = directions[d]
     return (x + x_delta, y + y_delta)
 
 
 def shortest_distance_to_origin(pos):
     x, y = pos
-    if (x > 0 and y > 0) or (x < 0 and y < 0):
-        return (min(abs(x), abs(y))
-                 + max(abs(x), abs(y)) - min(abs(x), abs(y)))
-    else:
-        return x + y
+    if x < 0 and y < 0: x, y = abs(x), abs(y)
+    if x > 0 and y > 0: return min(x, y) + max(x, y) - min(x, y)
+    else: return x + y
 
 
 if __name__ == '__main__':
     with open('input', 'r') as f:
         for line in f:
-            for direction in line.split(','):
-                xpos, ypos = move(xpos, ypos, direction.strip())
-                maxdist = max(maxdist,
-                        shortest_distance_to_origin((xpos, ypos)))
+            for d in line.split(','):
+                x, y = move(x, y, d.strip())
+                maxd = max(maxd, shortest_distance_to_origin((x, y)))
 
     print('part 1: shortest path to origin from {0}, {1} has distance {2}'
-            .format(xpos, ypos, shortest_distance_to_origin((xpos, ypos))))
+            .format(xpos, ypos, shortest_distance_to_origin((x, y))))
 
     print('part 2: maximum shortest path to origin had distance {0}'
-            .format(maxdist))
+            .format(maxd))
 
