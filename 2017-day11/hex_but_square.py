86cbf0f6e13522dd0a36023844803ec5fa3c1c99
Bennett Perkins <bennettperkins@gmail.com>
2017-12-11 15:58:25 +0800

    simplify: min(x,y) - min(x y) cancels out

--- 2017-day11/hex_but_square.py
+++ 2017-day11/hex_but_square.py
@@ -14,7 +14,7 @@ def move(x, y, d):
 def shortest_distance_to_origin(pos):
     x, y = pos
     if x < 0 and y < 0: x, y = abs(x), abs(y)
-    if x > 0 and y > 0: return min(x, y) + max(x, y) - min(x, y)
+    if x > 0 and y > 0: return max(x, y)
     else: return x + y
 
 
